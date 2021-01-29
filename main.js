5a3aacae289083cea0ddba91a0826f74d211d279
tizianocoppoli <tiziano.coppoli@gmail.com>
2021-01-29 09:45:51 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const baseURL = "https://swapi.dev/api/";
+
+function getData(type, cb) {
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    let tableHeaders = [];
+
+    Object.keys(obj).forEach(function (key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+
+    let el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function (data) {
+        let tableRows = [];
+        data = data.results;
+        let tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function (item) {
+            let dataRow = [];
+
+            Object.keys(item).forEach(function (key) {
+                let rowData = item[key].toString();
+                let truncatedData = rowData.substring(0,15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
